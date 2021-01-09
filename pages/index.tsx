ba69367d1f3fd2c9e11a3765c34ced8a01b5e55a
idonlakyo <daniel.lisq12@gmail.com>
2021-01-08 21:25:36 -0500

    added github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Navbar from 'react-bootstrap/Navbar';
 import CodeForm from '../components/CodeForm';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
 
 export default function Home() {
   return (
@@ -9,7 +11,10 @@ export default function Home() {
         <title>Re-Indenter</title>
       </Head>
       <Navbar bg="dark" variant="dark" expand="lg" className="justify-content-center">
-        <Navbar.Brand>Re-Indenter</Navbar.Brand>
+        <Navbar.Brand style={{marginLeft: "auto", marginRight: "auto"}}>Re-Indenter</Navbar.Brand>
+        <Button style-={{position: "absolute", marginRight: "0.5rem"}} href="https://github.com/danielsqli/reindenter">
+          Github
+        </Button>
       </Navbar>
       <CodeForm />
     </div>
